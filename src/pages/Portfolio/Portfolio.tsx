5a6af48f3802200873b6d78f59349970d033d372
lee <ljyeon0713@naver.com>
2023-03-17 10:00:04 +0900

    design: portfolio page publishing

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -2,16 +2,27 @@ import Footer from '../../components/Footer/Footer';
 import styles from './Portfolio.module.scss';
 import home from '../../assets/home.png';
 import arrow from '../../assets/arrowBottom.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 function Portfolio() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [title, setTitle] = useState('');
   const [subPageOpen, setSubPageOpen] = useState(false);
   const [tabOpen, setTabOpen] = useState(false);
+  useEffect(() => {
+    let params = new URL(document.URL).searchParams;
+    if (!params.get('tab') || params.get('tab') == '0') setTitle('펀드운용');
+    else if (params.get('tab') == '1') setTitle('투자현황');
+    else if (params.get('tab') == '2') setTitle('esg 투자 실천');
+    else if (params.get('tab') == '3') setTitle('펀드 운용 현황');
+  }, [location]);
   return (
     <div className={styles.container}>
       <section className={styles.banner}>
         <div className={styles.contentArea}>
           <h3>Portfolio</h3>
-          <h4>펀드운용</h4>
+          <h4>{title}</h4>
           <nav>
             <div className={styles.homeBtn}>
               <img src={home} />
@@ -25,22 +36,27 @@ function Portfolio() {
               <span>PORTFOLIO</span>
               <img src={arrow} />
               <ul className={styles.subList}>
-                <li>ABOUT</li>
-                <li>TEAM</li>
-                <li>IR</li>
-                <li>ABOUT</li>
+                <li onClick={() => navigate('/about')}>ABOUT</li>
+                <li onClick={() => navigate('/team')}>TEAM</li>
+                <li onClick={() => navigate('/ir')}>IR</li>
+                <li onClick={() => navigate('/family')}>FAMILY</li>
               </ul>
             </div>
             <div
               className={`${styles.nowTabWrap} ${tabOpen ? styles.isOpen : ''}`}
+              onClick={() => setTabOpen((prev) => !prev)}
             >
-              <span>펀드운용</span>
+              <span>{title}</span>
               <img src={arrow} />
               <ul className={styles.subList}>
-                <li>펀드운용</li>
-                <li>투자현황</li>
-                <li>esg 투자 실천</li>
-                <li>펀드 운용 현황</li>
+                <li onClick={() => navigate('/portfolio?tab=0')}>펀드운용</li>
+                <li onClick={() => navigate('/portfolio?tab=1')}>투자현황</li>
+                <li onClick={() => navigate('/portfolio?tab=2')}>
+                  esg 투자 실천
+                </li>
+                <li onClick={() => navigate('/portfolio?tab=3')}>
+                  펀드 운용 현황
+                </li>
               </ul>
             </div>
           </nav>
