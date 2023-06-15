5a8e8facce5acde2423881dd1faa63fc0696232b
lee <ljyeon0713@naver.com>
2023-06-15 15:06:49 +0900

    refactor: vc 수정 요구사항 적용

--- src/libs/axiosClient.ts
+++ src/libs/axiosClient.ts
@@ -3,10 +3,6 @@ const axiosClient = axios.create({
   // baseURL: '',
   baseURL: process.env.REACT_APP_API_URL,
   // withCredentials: true,
-  headers: {
-    // 'Access-Control-Allow-Origin': 'https://67cc-112-169-5-244.jp.ngrok.io',
-    'ngrok-skip-browser-warning': '69420',
-  },
 });
 
 export default axiosClient;
