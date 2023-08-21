47cd8982742be1ac8a95ead450678d65a1dcdd74
lee <ljyeon0713@naver.com>
2023-08-21 15:04:00 +0900

    refactor: 메인 용량 최적화

--- src/components/Test.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-function Test() {
-  const [number, setNumber] = useState(0);
-  return <div onClick={() => setNumber((prev) => prev + 1)}>{number}</div>;
-}
-export default Test;
